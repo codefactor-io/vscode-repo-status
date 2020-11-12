4d31fb6a54604a161416b5db5d903516be9e0198
Darius <darius@adroiti.com>
2020-11-12 10:21:22 +0200

    add basic failed state handling

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,29 @@ import * as vscode from 'vscode';
 import axios from 'axios';
 
 let statusBarItem: vscode.StatusBarItem;
+enum State {
+	Found,
+	NotFound,
+	NoFetchUrl
+}
+let itemState: State;
 
 export function activate({ subscriptions }: vscode.ExtensionContext) {
 
 	const command = 'codefactor.showRepoStats';
 	subscriptions.push(vscode.commands.registerCommand(command, () => {
-		vscode.env.openExternal(vscode.Uri.parse('https://www.codefactor.io'));
+		if (itemState == State.Found)
+		{
+			vscode.env.openExternal(vscode.Uri.parse('https://www.codefactor.io'));
+		}
+		else if (itemState == State.NotFound)
+		{
+			vscode.window.showWarningMessage(`Repository was not found on CodeFactor. Please make sure it's public and reviewed.`);
+		}
+		else if (itemState == State.NoFetchUrl)
+		{
+			vscode.window.showWarningMessage(`Working folder is not a git repository.`);
+		}
 	}));
 
 	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
@@ -24,6 +41,13 @@ function updateStatusBarItem(): void {
 
 	api.onDidChangeState(() => {
 		var repo = api.repositories[0];
+		if (repo == undefined)
+		{
+			statusBarItem.text = `$(megaphone) -`;
+			statusBarItem.show();
+			itemState = State.NoFetchUrl;
+			return;
+		}
 		var fetchUrl = repo.state.remotes[0]?.fetchUrl;
 		var branchName = repo.state.HEAD.name;
 
@@ -32,21 +56,29 @@ function updateStatusBarItem(): void {
 			try {
 				var url = `https://www.codefactor.io/home/repoMeta?cloneUrl=${fetchUrl}&branchName=${branchName}`;
 				axios.get(url).then((resp) => {
-					if (resp.status == 200)
+					if (resp.status == 200 && resp.data?.issueCount != null)
 					{
 						statusBarItem.text = `$(megaphone) ${resp.data.grade}, ${resp.data.issueCount} issues`;
 						statusBarItem.show();
+						itemState = State.Found;
 					}
-					else {
-						statusBarItem.hide();
+					else 
+					{
+						statusBarItem.text = `$(megaphone) -`;
+						statusBarItem.show();
+						itemState = State.NotFound;
 					}
 				});
 			} catch (exception) {
-				statusBarItem.hide();
+				statusBarItem.text = `$(megaphone) -`;
+				statusBarItem.show();
+				itemState = State.NotFound;
 			}
 		}
 		else {
-			statusBarItem.hide();
+			statusBarItem.text = `$(megaphone) -`;
+			statusBarItem.show();
+			itemState = State.NoFetchUrl;
 		}
 	})
 }
